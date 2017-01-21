3aa0de8d96545e26c8ed0f62309332de7ba45b83
Work-PC\Work <gftvfx@gmail.com>
2017-01-21 14:14:40 -0800

    Lecture 116: C++ overrides for Begin play and possesing player controller.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -4,8 +4,25 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    auto ControlledTank = GetControlledTank();
+
+    if (!ControlledTank)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController not possesing a tank"));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Warning, TEXT("PlayerController possesing %s"), *(ControlledTank->GetName()));
+    }
+}
+
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+    UE_LOG(LogTemp, Warning, TEXT("Tank Possessed"));
 }
