6190efe82741139bb0fae5077ecda034b6d94a9a
Work-PC\Work <gftvfx@gmail.com>
2017-01-29 10:43:03 -0800

    Lecture 143: AI tanks are firing now. Set up some timers and booleans to simulate a reload time so that there is a limit to how fast all tanks can fire.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -49,15 +49,20 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-    if (!Barrel) { return; }
+    bool isReloaded = (FPlatformTime::Seconds() - LastFireTime > ReloadTimeInSeconds);
 
-    // Spawn projectile at barrel location
-    auto Projectile = GetWorld()->SpawnActor<AProjectile>
-    (
-        ProjectileBlueprint,
-        Barrel->GetSocketLocation(FName("Projectile")),
-        Barrel->GetSocketRotation(FName("Projectile"))
-    );
+    if (Barrel && isReloaded)
+    {
 
-    Projectile->LaunchProjectile(LaunchSpeed);
+        // Spawn projectile at barrel location
+        auto Projectile = GetWorld()->SpawnActor<AProjectile>
+        (
+            ProjectileBlueprint,
+            Barrel->GetSocketLocation(FName("Projectile")),
+            Barrel->GetSocketRotation(FName("Projectile"))
+        );
+
+        Projectile->LaunchProjectile(LaunchSpeed);
+        LastFireTime = FPlatformTime::Seconds();
+    }
 }
