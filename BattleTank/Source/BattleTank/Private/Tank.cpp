f9bfe604b3080571190e45fb125f6339321297e2
Work-PC\Work <gftvfx@gmail.com>
2017-02-04 14:58:38 -0800

    Lecture 151: Removed TankMovementComponent as a DefaultSubComponent of Tank and added it as a BluePrint component. Bound input to LeftTrack and RightTrack for fly-by-wire type control.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -16,7 +16,6 @@ ATank::ATank()
 
     // No need to protect pointers as they are added at construction
     TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-    TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 void ATank::SetBarrelReference(UTankBarrel* BarrelToSet)
--- BattleTank/Source/BattleTank/Public/TankMovementComponent.h
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -5,8 +5,10 @@
 #include "GameFramework/NavMovementComponent.h"
 #include "TankMovementComponent.generated.h"
 
+class UTankTrack;
+
 /**
- * 
+ * Resoponsible for driving the tank tracks
  */
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
@@ -18,4 +20,10 @@ public:
     UFUNCTION(BlueprintCallable, Category = Input)
     void IntendMoveForward(float Throw);
 	
+    UFUNCTION(BlueprintCallable, Category = Setup)
+    void Initialize(UTankTrack* LefTrackToSet, UTankTrack* RightTrackToSet);
+
+private:
+    UTankTrack* LeftTrack = nullptr;
+    UTankTrack* RightTrack = nullptr;
 };
