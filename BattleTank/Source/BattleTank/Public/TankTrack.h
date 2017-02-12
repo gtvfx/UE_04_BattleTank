c63ca9a85faf51a4accb74223140612e17fcd5c4
Work-PC\Work <gftvfx@gmail.com>
2017-02-12 09:48:15 -0800

    Lecture 174: Registered an OnHit event with the TankTrack with C++

--- BattleTank/Source/BattleTank/Public/TankTrack.h
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -26,5 +26,10 @@ private:
 
 	UTankTrack();
 
+	virtual void BeginPlay() override;
+
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFuncion) override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
